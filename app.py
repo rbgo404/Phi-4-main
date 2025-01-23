ae01013344ea2bd86add798c74caaede2cd63a3c
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2025-01-24 01:32:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,10 +25,8 @@ class InferlessPythonModel:
         self.tokenizer = AutoTokenizer.from_pretrained(model_id)
 
     def infer(self, request: RequestObjects) -> ResponseObjects:
-        sampling_params = SamplingParams(temperature=temperature,top_p=top_p,
-                                         repetition_penalty=repetition_penalty,
-                                         top_k=top_k,max_tokens=max_tokens
-                                        )
+        sampling_params = SamplingParams(temperature=request.temperature,top_p=request.top_p,repetition_penalty=request.repetition_penalty,
+                                         top_k=request.top_k,max_tokens=request.max_tokens)
         input_text = self.tokenizer.apply_chat_template([{"role": "user", "content": prompts}], tokenize=False)
         result = self.llm.generate(input_text, sampling_params)
         result_output = [output.outputs[0].text for output in result]
